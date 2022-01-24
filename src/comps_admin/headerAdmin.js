443f8789ed5269af7d740adc32739487746e2d96
shimondriham <95618903+shimondriham@users.noreply.github.com>
2022-01-24 11:44:14 +0200

    aa

--- src/comps_admin/headerAdmin.js
+++ src/comps_admin/headerAdmin.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HeaderAdmin(props){
   return(
-    <div>HeaderAdmin work</div> 
+    <div className='header_admin container-fluid bg-dark d-flex align-items-center'>
+      <h2 className='text-white col-auto me-4'>Admin panel</h2>
+      <nav className='col-md-9'>
+        <Link to="/admin/products" >Products</Link>
+        <Link to="/admin/categories" >Categories</Link>
+        <Link to="/admin/users" >Users</Link>
+      </nav>
+    </div> 
   )
 }
 
