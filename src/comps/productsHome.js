57fd397c60e1b748fc90cd27db139cf72c559690
shimondriham <95618903+shimondriham@users.noreply.github.com>
2022-02-02 15:34:50 +0200

    aa

--- src/comps/productsHome.js
+++ src/comps/productsHome.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL, doApiGet } from '../services/apiService';
-import { checkVisitedLocal } from '../services/localService';
+import { checkVisitedLocal, VISITED_PRODUCT } from '../services/localService';
 import ProducItem from './productItem';
 
 function ProductsHome(props){
@@ -28,7 +28,12 @@ function ProductsHome(props){
 
   return(
     <div className='container py-4'>
-      <h2 className='text-center'>New products in our store</h2>
+      {localStorage[VISITED_PRODUCT] ? 
+        <h2 className='text-center text-info'>Products you intersted in</h2> :
+      <h2 className='text-center'>
+        New products in our store
+      </h2>
+    }
       <div className="row">
         {ar.map(item => {
           return(
--- src/comps/productsListPage.js
+++ src/comps/productsListPage.js
@@ -30,7 +30,7 @@ function ProductsListPage(props){
   }
 // TODO: ADD PAGENATION
   return(
-    <div className='container-fluid' style={{minHeigth:"90vh"}}>
+    <div className='container-fluid' style={{minHeight:"85vh"}}>
       <div className="container">
         <h1 className='text-center'>Categories of {params.cat_url}</h1>
         {ar.length == 0 ? <h2>Loading...</h2> : ""}
