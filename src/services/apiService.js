9475a1755dad6ad1cc1207aa8074ab5b9aefff73
shimondriham <95618903+shimondriham@users.noreply.github.com>
2022-02-28 13:11:23 +0200

    aa

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_URL = "http://localhost:3002";
-export const API_URL = "http://shimonshop.herokuapp.com";
+export const API_URL = "http://localhost:3002";
+// export const API_URL = "http://shimonshop.herokuapp.com";
 
 
 
